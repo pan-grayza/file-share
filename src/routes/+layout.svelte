3e02d2dc231dc5558b8cf24841e2824b5af48a30
Pan-Grayza <zagraystepan@gmail.com>
2024-08-03 10:09:21 +0100

    Configured styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<div class="flex flex-row w-full h-full">
+    <nav class="flex flex-row w-32 h-full border-r-2">
+        <a href="/">home</a>
+        <a href="/blog">blog</a>
+    </nav>
+
+    <slot />
+</div>
